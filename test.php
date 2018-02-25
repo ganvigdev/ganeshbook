458b49a9ae6f78a52a5ea638b1d6bd9f6dcf636a
Ravindranath Sangu <ravindranath.sangu@gmail.com>
2018-02-25 12:25:34 +0530

    My first commmit

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysql_connect('localhost','ganeshbo_db','14Ove5G3I1q7') or die(mysql_error());
+mysql_select_db('ganeshbo_sample',$conn);
+$arrResult = mysql_fetch_object(mysql_query("select * from users where user_id=2"));
+echo '<pre>';
+print_r($arrResult);
+?>
