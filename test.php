6448f664106259388a6751e754a8bafa0deb5848
Ravindranath Sangu <ravindranath.sangu@gmail.com>
2018-02-25 15:34:40 +0530

    In test.php added comment

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 $conn = mysql_connect('localhost','ganeshbo_db','14Ove5G3I1q7') or die(mysql_error());
 mysql_select_db('ganeshbo_sample',$conn);
 $arrResult = mysql_fetch_object(mysql_query("select * from users where user_id=2"));
+//Printing array
 echo '<pre>';
 print_r($arrResult);
 ?>
