ccfec01c5841f9766ac6cd5d7e274e00e2c1a796
Ravindranath Sangu <ravi@inventcorp.com>
2018-03-24 11:30:54 +0530

    index page updated for favicon

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
         <title>GaneshBook.com In Subdomain of ravi</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
+		<link rel="shortcut icon" href="favicon.ico" />
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <style>
             body {
